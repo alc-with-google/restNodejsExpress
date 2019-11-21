2488d65c0ad2d85929621a293566939306e228ea
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2019-11-21 14:50:41 +0100

    Implementing Http Get

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 
 const app = express();
-
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    //creating the response
+    const response = { hello: 'This is my API' };
+
+    //sending the response
+    res.json(response)
+
+    //we could have res.sendFile(); res.render(); res.send()
+  });
+
+app.use('/api', bookRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to my Nodemon API!');
